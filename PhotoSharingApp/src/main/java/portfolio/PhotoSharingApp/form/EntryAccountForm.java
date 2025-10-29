4432cd15289ad59b6d5441bf59d0944ed0f680b7
myghjp <mscmkmy@outlook.com>
2025-10-29 11:27:15 +0900

    バリデーションとアノテーションの追加中

--- PhotoSharingApp/src/main/java/portfolio/PhotoSharingApp/form/EntryAccountForm.java
+++ PhotoSharingApp/src/main/java/portfolio/PhotoSharingApp/form/EntryAccountForm.java
@@ -3,40 +3,28 @@ package portfolio.PhotoSharingApp.form;
 import org.hibernate.validator.constraints.Length;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class EntryAccountForm {
 	
-	@NotNull
+	@NotBlank
 	@Length(max = 50)
 	private String user;
 	
-	/*50文字以内である,
-	 * 登録済のアカウント名と重複していない,
-	 * 入力されている*/
+	/*登録済のアカウント名と重複していない*/
 	/*ーーーーーーーーーーーーーーーーーーー*/
-	
-	/*正規表現しかない？*/
-	
-	
-	@NotNull
-	@Size(min = 4, max = 12)
+	@NotBlank
+	@Pattern(regexp = "^[a-zA-Z0-9]+$")
+	@Length(min = 8, max = 12)
 	private String pass;
-	/*半角英数字のみ,
-	 * 8文字以上12文字以下,
-	 * 入力されている*/
 	/*ーーーーーーーーーーーーーーーーーーー*/
+	@NotBlank
 	
-	/*EmailValidator？？？*/
-	
-	@NotNull
 	@Email
 	private String email_address;
-	/*メールアドレスの形式であること,
-	 * 登録済のアドレスと重複していない,
-	 * 入力されている*/
+	/*登録済のアカウント名と重複していない*/
 	/*ーーーーーーーーーーーーーーーーーーー*/
 }
