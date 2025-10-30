cfb1da3032c36de574af0301e1375d767339445f
myghjp <mscmkmy@outlook.com>
2025-10-30 15:51:48 +0900

    画面分類( D グループ ) を少し追加

--- PhotoSharingApp/src/main/java/portfolio/PhotoSharingApp/controller/EntryGroupController.java
+++ PhotoSharingApp/src/main/java/portfolio/PhotoSharingApp/controller/EntryGroupController.java
@@ -4,15 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class SelectGroupController {
-
-	@GetMapping("/select-group")
-	public String getLogin() {
+public class EntryGroupController {
+	
+	@GetMapping("/entry-group")
+	public String getEntryGroup() {
 		
 		/*※サービスからグループリストの呼び出し
 		List<TodoItems> todoItemsList = todoService.getListAll();
 		model.addAttribute("todoItemsList", todoItemsList);*/
 		
-		return "select-group";
+		return "entry-group";
 	}
+
 }
