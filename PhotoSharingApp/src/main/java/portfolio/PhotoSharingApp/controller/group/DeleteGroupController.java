a0cecacafb5b4cceb53a77642093f368e5d4744b
myghjp <mscmkmy@outlook.com>
2025-11-04 15:44:44 +0900

    formとidの整理/マージ

--- PhotoSharingApp/src/main/java/portfolio/PhotoSharingApp/controller/group/DeleteGroupController.java
+++ PhotoSharingApp/src/main/java/portfolio/PhotoSharingApp/controller/group/DeleteGroupController.java
@@ -1,15 +1,33 @@
 package portfolio.PhotoSharingApp.controller.group;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class DeleteGroupController {
-	
-	@GetMapping("/delete-group")
-	public String getDeleteGroup() {
+
+	@GetMapping("/delete-group/{id}")
+	public String getDeleteGroup(Model model, @PathVariable("id") int id) {
+
+		/*idとname*/
+		/*idからname*/
 		
+		model.addAttribute(id);
+
 		return "group/delete-group";
 	}
+
+	/*@PostMapping("/delete-group")
+	public String postDeleteGroup(
+			RedirectAttributes redirectAttributes) {
+		
+		
+		
+		idのみ
+		
+		return "redirect:group/delete-group";
+	}*/
 
 }
