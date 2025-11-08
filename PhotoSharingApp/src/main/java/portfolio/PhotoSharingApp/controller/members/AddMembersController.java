91663426d79e98f715ec70ce241319cabf41a232
myghjp <mscmkmy@outlook.com>
2025-11-08 14:43:30 +0900

    エラークラスに例外発生時のsession破棄を追加

--- PhotoSharingApp/src/main/java/portfolio/PhotoSharingApp/controller/members/AddMembersController.java
+++ PhotoSharingApp/src/main/java/portfolio/PhotoSharingApp/controller/members/AddMembersController.java
@@ -1,25 +1,23 @@
 package portfolio.PhotoSharingApp.controller.members;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import portfolio.PhotoSharingApp.entity.Groups;
-import portfolio.PhotoSharingApp.entity.Members;
 import portfolio.PhotoSharingApp.form.members.AddMembersForm;
 
 @Controller
 @SessionAttributes(value = {"groups"})
 public class AddMembersController {
 	
-	@Autowired
-	private ModelMapper modelMapper;
+	/*@Autowired
+	private ModelMapper modelMapper;*/
 	
 	@GetMapping("/add-members")
 	public String getAddMembers(Model model
@@ -34,34 +32,40 @@ public class AddMembersController {
 	@PostMapping("/add-members")
 	public String postAddMembers(Model model
 			,Groups groups
+			,@RequestParam("emailAddress") String emailAddress
 			,@ModelAttribute AddMembersForm addMembersForm
 			,RedirectAttributes redirectAttributes
 		) {
-		/*※メールアドレスを登録するわけではない*/
-		Members members = modelMapper.map(addMembersForm, Members.class);
-		
-		
-		
-		/*メールアドレスの変数↑*/
 		/*ーーーーーーーーーーーーーーーーーー*/
-		/*入力されている*/
-		/*メールアドレスの形式である*/
+		/*↓バリデーションを同時作成する必要がある*/
+		/*ーーーーーーーーーーーーーーーーーー*/
+		
+		/*変換*/
+		/*Members members = modelMapper.map(addMembersForm, Members.class);*/
+		
+		/*※メールアドレスを登録するわけではない*/
+		
 		/*ーーーーーーーーーーーーーーーーーー*/
 		/*if (userService.isExistingAccountsData2(accounts)) {
 			bindingResult.rejectValue("email_address", "email_address.Alert");
 		}*/
-		/*追加するアドレスがアカウント登録されていること,*/
-		/*※※追加済の同じグループのメンバのアドレスと重複していないこと,*/
+		/*追加するアドレスがアカウント登録されていること(アカウントIDが存在するか)*/
+		/*追加済の同じグループのメンバのアドレスと重複していないこと
+		 * (メンバリストのアカウントIDとアカウントテーブルIDの比較？？？)*/
 		/*ーーーーーーーーーーーーーーーーーー*/
-		
-		/*このグループのIDは？*/
-		/*メールアドレスからアカウントIDを取得*/
-		
+		/*if (bindingResult.hasErrors()) {
+			return getEntryAccount(model, entryAccountForm);
+		}*/
+		/*入力されている*/
+		/*メールアドレスの形式である*/
 		/*ーーーーーーーーーーーーーーーーーー*/
 		/*バリデーションを終えると
+		メールアドレスを使用してアカウントIDを取得する(バインドとまとめる)
+		
 		membersのテーブルに
-		グループのIDと
-		アカウントのIDを追加する*/
+		sessionのグループのIDと
+		取得したアカウントのIDを追加する*/
+		/*ーーーーーーーーーーーーーーーーーー*/
 		return "redirect:list-members";
 	}
 }
