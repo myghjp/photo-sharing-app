c90783657d2d271ac929e09a94bcbbe3be757314
myghjp <mscmkmy@outlook.com>
2025-11-24 15:49:00 +0900

    H.写真系作成前

--- PhotoSharingApp/src/main/java/portfolio/PhotoSharingApp/controller/album/SelectAlbumController.java
+++ PhotoSharingApp/src/main/java/portfolio/PhotoSharingApp/controller/album/SelectAlbumController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -32,5 +34,38 @@ public class SelectAlbumController {
 		model.addAttribute("albumList", albumList);
 		
 		return "album/select-album";
+	}
+	
+	@PostMapping("/select-album")
+	public String postSelectAlbum(Model model
+			,Groups groups
+			,@RequestParam("id")int albumId
+			,RedirectAttributes redirectAttributes) {
+		
+		int groupId = groups.getId();
+		
+		
+		
+		/*アルバムもセッション？*/
+		
+		/*取得したアルバムIDと
+		使用しているグループIDを使い
+		写真一覧を取得する*/
+		
+		
+		/*※ここは表示*/
+		/*グループ名*/
+		/*アルバム名*/
+		
+		
+		
+		
+		
+		/*model.addAttribute("groupName",groups.getGroupName());*/
+		
+		/*	List<Albums> albumList = albumService.getAlbumList();*/
+		/*model.addAttribute("albumList", albumList);*/
+		
+		return "redirect:list-photo";
 	}
 }
--- PhotoSharingApp/src/main/java/portfolio/PhotoSharingApp/controller/photo/ListPhotoController.java
+++ PhotoSharingApp/src/main/java/portfolio/PhotoSharingApp/controller/photo/ListPhotoController.java
@@ -1,21 +1,17 @@
 package portfolio.PhotoSharingApp.controller.photo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class ListPhotoController {
 	
 	@GetMapping("/list-photo")
-	public String getListPhoto(Model model) {
+	public String getListPhoto(RedirectAttributes redirectAttributes
+			) {
 		
-		
-		/*取得したアルバムIDと
-		使用しているグループIDを使い
-		写真一覧を取得する*/
-		
-		/*グループ名も必要*/
+	
 		
 		return "photo/list-photo";
 	}
