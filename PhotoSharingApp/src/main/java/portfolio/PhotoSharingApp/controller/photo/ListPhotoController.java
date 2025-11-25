eb0b79b2c7f798cd7302644d80f5f451ca7954d1
myghjp <mscmkmy@outlook.com>
2025-11-25 14:16:25 +0900

    photo系のserviceとmapperを作成

--- PhotoSharingApp/src/main/java/portfolio/PhotoSharingApp/controller/photo/ListPhotoController.java
+++ PhotoSharingApp/src/main/java/portfolio/PhotoSharingApp/controller/photo/ListPhotoController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,14 +14,15 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import portfolio.PhotoSharingApp.entity.Groups;
+import portfolio.PhotoSharingApp.entity.Photos;
+import portfolio.PhotoSharingApp.service.photo.PhotoService;
 
 @Controller
 @SessionAttributes(value = {"groups"})
 public class ListPhotoController { 
 	
-	/*@Autowired
-	private PhotoService photoService;*/
+	@Autowired
+	private PhotoService photoService;
 	
 	@GetMapping("/list-photo")
 	public String getListPhoto(Model model
@@ -36,7 +38,7 @@ public class ListPhotoController {
 	
 	@PostMapping("/list-photo")
 	public String postListPhoto(
-			Groups groups
+			Photos photos
 			,@RequestParam("photo")MultipartFile photo
 			,RedirectAttributes redirectAttributes
 			)throws IOException {
---
+++ PhotoSharingApp/src/main/java/portfolio/PhotoSharingApp/repository/PhotoMapper.java
@@ -0,0 +1,8 @@
+package portfolio.PhotoSharingApp.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PhotoMapper {
+
+}
