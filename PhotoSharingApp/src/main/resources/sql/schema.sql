7b87f5b7a2aab52db3387bcb91ab54e0077a7c4f
myghjp <mscmkmy@outlook.com>
2026-02-19 15:08:30 +0900

    DataSource追加/ダミーSQLの記入

--- PhotoSharingApp/src/main/resources/sql/schema.sql
+++ PhotoSharingApp/src/main/resources/sql/schema.sql
@@ -1,68 +1,78 @@
-CREATE DATABASE IF NOT EXISTS photo_app
-DEFAULT CHARACTER SET utf8mb4 
-COLLATE utf8mb4_0900_ai_ci;
+DROP DATABASE IF EXISTS photo_app;
+CREATE DATABASE IF NOT EXISTS photo_app;
+USE photo_app;
 
-CREATE TABLE IF NOT EXISTS accounts(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user VARCHAR(20) NOT NULL,
-    pass VARCHAR(255) NOT NULL,
-    email_address VARCHAR(255) NOT NULL,
-    create_date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    UNIQUE KEY unique_user (user),
-    UNIQUE KEY unique_email (email_address)
-);
+CREATE TABLE `accounts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user` varchar(20) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `email_address` varchar(255) NOT NULL,
+  `create_date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_user` (`user`),
+  UNIQUE KEY `unique_email` (`email_address`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS `groups`(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    group_name VARCHAR(20) NOT NULL,
-    account_id INT NOT NULL,
-    create_date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    UNIQUE KEY (group_name),
-    INDEX (account_id)
-);
+CREATE TABLE `groups` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `group_name` varchar(20) NOT NULL,
+  `account_id` int NOT NULL,
+  `create_date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `group_name` (`group_name`),
+  KEY `account_id` (`account_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS groups_members(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    group_id INT NOT NULL,
-    account_id INT,
-    create_date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    INDEX (account_id),
-    FOREIGN KEY (group_id) REFERENCES `groups` (id) ON DELETE CASCADE,
-    FOREIGN KEY (account_id) REFERENCES accounts (id) ON DELETE SET NULL
-);
+CREATE TABLE `groups_members` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `group_id` int NOT NULL,
+  `account_id` int DEFAULT NULL,
+  `create_date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `account_id` (`account_id`),
+  KEY `group_id` (`group_id`),
+  CONSTRAINT `groups_members_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `groups_members_ibfk_2` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS comments(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    group_id INT NOT NULL,
-    comment VARCHAR(50) NOT NULL,
-    account_id INT,
-    create_date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    INDEX (account_id),
-    FOREIGN KEY (group_id) REFERENCES `groups`(id) ON DELETE CASCADE,
-    FOREIGN KEY (account_id) REFERENCES accounts(id) ON DELETE SET NULL
-);
+CREATE TABLE `comments` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `group_id` int NOT NULL,
+  `comment` varchar(50) NOT NULL,
+  `account_id` int DEFAULT NULL,
+  `create_date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `account_id` (`account_id`),
+  KEY `group_id` (`group_id`),
+  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS albums(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    group_id INT NOT NULL,
-    album_name VARCHAR(20) NOT NULL,
-    create_date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (group_id) REFERENCES `groups`(id) ON DELETE CASCADE
-);
+CREATE TABLE `albums` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `group_id` int NOT NULL,
+  `album_name` varchar(20) NOT NULL,
+  `create_date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `group_id` (`group_id`),
+  CONSTRAINT `albums_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS photos(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    album_id INT NOT NULL,
-    photo VARCHAR(255) NOT NULL,
-    account_id INT,
-    create_date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    INDEX (account_id),
-    FOREIGN KEY (album_id) REFERENCES albums(id) ON DELETE CASCADE,
-    FOREIGN KEY (account_id) REFERENCES accounts(id) ON DELETE SET NULL
-);
+CREATE TABLE `photos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `album_id` int NOT NULL,
+  `photo` varchar(255) NOT NULL,
+  `account_id` int DEFAULT NULL,
+  `create_date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `account_id` (`account_id`),
+  KEY `album_id` (`album_id`),
+  CONSTRAINT `photos_ibfk_1` FOREIGN KEY (`album_id`) REFERENCES `albums` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `photos_ibfk_2` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
